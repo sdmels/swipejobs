78855d476408daf3f3b99bfdf53462ce0e8208e1
Daniel Susaimanickam <sdmels@outlook.com>
2020-04-20 19:06:40 +1000

    fixed the type of the header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, RootStateOrAny } from 'react-redux';
 
 import logo from 'assets/logo.png';
 import './Header.scss';
 
 const Header = () => {
-  const fullName = useSelector((state: any) => `${state.users.firstName} ${state.users.lastName}`);
+  const fullName = useSelector(
+    (state: RootStateOrAny) => `${state.users.firstName} ${state.users.lastName}`,
+  );
   if (!fullName) {
     return null;
   }
